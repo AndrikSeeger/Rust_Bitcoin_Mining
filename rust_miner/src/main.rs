e284965163e37410c25b171dc0182dbd1755e550
Andrik Seeger <72473553+AndrikSeeger@users.noreply.github.com>
2022-07-03 13:09:02 +0200

    Update main.rs

--- rust_miner/src/main.rs
+++ rust_miner/src/main.rs
@@ -1,9 +1,5 @@
 use lib::*;
-// Startet das mining
 fn main(){
-    // Input1 Accountname von Slushpool
-    // Slushpooladresse
-    // Workername kann beliebig sein
     let mut conn=PoolConnection::new("ITA_Miner","eu.stratum.slushpool.com:3333","worker1");
     conn.handle_datastream();
 }
