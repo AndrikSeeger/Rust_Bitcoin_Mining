f664d00ec1ebb6e32298a83a0420137d1a114054
Andrik Seeger <72473553+AndrikSeeger@users.noreply.github.com>
2022-07-03 13:03:18 +0200

    Update main.rs

--- rust_miner/src/main.rs
+++ rust_miner/src/main.rs
@@ -5,6 +5,5 @@ fn main(){
     // Slushpooladresse
     // Workername kann beliebig sein
     let mut conn=PoolConnection::new("ITA_Miner","eu.stratum.slushpool.com:3333","worker1");
-    // Eigentlich sollte asynchron mit mehrern Threads gearbeitet werden aber zu wenig ZEit
     conn.handle_datastream();
 }
